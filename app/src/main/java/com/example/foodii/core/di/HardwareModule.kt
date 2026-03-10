38f4e364433fd00ef9e359dbc66087e52611f076
Hannia Paola De Los Santos Bautista <233273@ids.upchiapas.edu.mx>
2026-03-10 04:20:01 -0600

    Add SD

---
+++ app/src/main/java/com/example/foodii/core/di/HardwareModule.kt
@@ -0,0 +1,22 @@
+package com.example.foodii.core.di
+
+import android.content.Context
+import com.example.foodii.core.hardware.data.AndroidShakeDetector
+import com.example.foodii.core.hardware.domain.ShakeDetector
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HardwareModule {
+
+    @Provides
+    @Singleton
+    fun provideShakeDetector(@ApplicationContext context: Context): ShakeDetector {
+        return AndroidShakeDetector(context)
+    }
+}
